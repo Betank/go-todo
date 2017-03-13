77119fd2490f347bd44a37d00b198b8fb639d8af
Benjamin Tank <tank@optimal-systems.de>
2017-03-13 14:25:08 +0100

    rendering html on server side

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("")))
+
+	mux := mux.NewRouter()
+	mux.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
+	mux.HandleFunc("/", ServeIndexHTML)
+
+	http.Handle("/", mux)
+	http.ListenAndServe(":8080", nil)
+}
+
+// ServeIndexHTML renders and serves page
+func ServeIndexHTML(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles(filepath.Join("templates", "index.tmpl")))
+	t.Execute(w, nil)
 }
