353b0d99d546e097b115919ad79c0918bed2c0da
Betank <bennitank@gmail.com>
2017-03-14 01:43:39 +0100

    add todolist entry struct and list elements to template

--- main.go
+++ main.go
@@ -4,11 +4,27 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"sync"
 
 	"github.com/gorilla/mux"
 )
 
+type storage struct {
+	sync.Mutex
+	todos []todo
+}
+
+type todo struct {
+	ID        string
+	Title     string
+	Completed bool
+	Checked   bool
+}
+
+var entries *storage
+
 func main() {
+	entries = &storage{todos: make([]todo, 0)}
 
 	mux := mux.NewRouter()
 	mux.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
@@ -21,5 +37,8 @@ func main() {
 // ServeIndexHTML renders and serves page
 func ServeIndexHTML(w http.ResponseWriter, r *http.Request) {
 	t := template.Must(template.ParseFiles(filepath.Join("templates", "index.tmpl")))
-	t.Execute(w, nil)
+
+	entries.Lock()
+	defer entries.Unlock()
+	t.Execute(w, entries.todos)
 }
